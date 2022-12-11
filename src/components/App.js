02772f76611f3c9ca1a7dec242e40b98b74572fe
Syed Saif Ali Shah <shahsaif99@gmai.com>
2022-12-12 02:50:29 +0500

    added more funcationalies

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,43 @@ import {nanoid} from "nanoid"
 
 export default function App(){
   const [dice, setDice]=React.useState(allNewDice())
+  function generateNewDie(){
+    return{
+      num: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+  
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-        newDice.push({
-          num:Math.ceil(Math.random() * 6),
-          isHeld:false,
-          id:nanoid()
-
-        
-        })
+        newDice.push(generateNewDie())
     }
     return newDice
 }
 function rollDice(){
-  return setDice(allNewDice())
+   return setDice(oldDice => oldDice.map(die => {
+    return die.isHeld ? 
+        die :
+        generateNewDie()
+}))
 }
-  const diceElements=dice.map(die=> <Die key={die.id} num={die.num}/>)
+function holdDice(id){
+  setDice(oldDice=> oldDice.map(die=>{
+    return die.id === id ? {...die, isHeld:!die.isHeld}: die
+  }))
+}
+  const diceElements=dice.map(die=> <Die 
+    key={die.id} 
+    num={die.num} 
+    isHeld={die.isHeld}
+    holdDice={()=>holdDice(die.id)}
+    />)
   return (
    <main>
+     <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="dice-container">
         {diceElements}
     
--- src/components/die.js
+++ src/components/die.js
@@ -1,9 +1,18 @@
 import React from "react"
 
 export default function Die(props){
+    const styles={
+        backgroundColor:props.isHeld? "#59E391": "white"
+    }
     return(
-        <div className="dice">
-            <div >{props.num}</div>
+        <div
+         className="dice" 
+         style={styles}
+         onClick={props.holdDice}
+         >
+            
+            <div  >
+                <div>{props.num}</div></div>
 
         </div>
     )
