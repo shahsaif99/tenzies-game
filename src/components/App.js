f77b3dfa9f20158ff05542dd3de551deb649c7d4
Syed Saif Ali Shah <shahsaif99@gmai.com>
2022-12-11 21:40:43 +0500

    added dice and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
 import React from "react"
-
+import Die from "./die"
 
 export default function App(){
+  
   return (
    <main>
-        
-        
-         
-        
+        <div className="dice-container">
+
+       
+         <Die
+        num= {1}
+        />
+        <Die
+        num= {1}
+        />
+        <Die
+        num= {1}
+        />
+        <Die
+        num= {1}
+        />
+        <Die
+        num= {1}
+        />
+        <Die
+        num= {1}
+        />
+        <Die
+        num= {1}
+        />
+        <Die
+        num= {1}
+        />
+        <Die
+        num= {1}
+        />
+        <Die
+        num= {1}
+        />
+        </div>
     
             </main>)
 }
---
+++ src/components/die.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Die(props){
+    return(
+        <div className="dice">
+            <div >{props.num}</div>
+
+        </div>
+    )
+}
