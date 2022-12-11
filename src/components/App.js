e42c3684349c740fb6696e456ea53695a4ec7e19
Syed Saif Ali Shah <shahsaif99@gmai.com>
2022-12-12 00:33:10 +0500

    changed dice array to object

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
 import React from "react"
 import Die from "./die"
+import {nanoid} from "nanoid"
 
 export default function App(){
   const [dice, setDice]=React.useState(allNewDice())
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-        newDice.push(Math.ceil(Math.random() * 6))
+        newDice.push({
+          num:Math.ceil(Math.random() * 6),
+          isHeld:false,
+          id:nanoid()
+
+        
+        })
     }
     return newDice
 }
 function rollDice(){
   return setDice(allNewDice())
 }
-  const diceElements=dice.map(die=> <Die num={die}/>)
+  const diceElements=dice.map(die=> <Die key={die.id} num={die.num}/>)
   return (
    <main>
         <div className="dice-container">
